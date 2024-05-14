979fd067ddc01aa3bd044ca032be210d2b9876c8
Smootheez <raehanstars@gmail.com>
2024-05-14 08:12:12 +0700

    Adding first action

--- src/main/java/net/smootheez/elytracontrol/ElytraControl.java
+++ src/main/java/net/smootheez/elytracontrol/ElytraControl.java
@@ -1,10 +1,32 @@
 package net.smootheez.elytracontrol;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.effect.StatusEffects;
 
+@Environment(EnvType.CLIENT)
 public class ElytraControl implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         Constant.LOGGER.info("Elytra Control Initialized");
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.player == null) {
+                return;
+            }
+
+            while (checkStopFlying(client)) {
+                client.player.stopFallFlying();
+            }
+        });
+    }
+
+    protected boolean checkStopFlying(MinecraftClient client) {
+        if (client.player == null) {
+            return false;
+        }
+        return client.options.jumpKey.wasPressed() && !client.player.isOnGround() && !client.player.isTouchingWater() && !client.player.hasStatusEffect(StatusEffects.LEVITATION);
     }
 }
