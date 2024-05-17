122db036f011650a80df97cf53580d844a2a4600
Smootheez <raehanstars@gmail.com>
2024-05-17 14:21:40 +0700

    Fixing logic for elytra to stop in midair and improving some translation text

--- src/main/java/net/smootheez/elytracontrol/ElytraControl.java
+++ src/main/java/net/smootheez/elytracontrol/ElytraControl.java
@@ -4,10 +4,10 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.client.MinecraftClient;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
 import net.minecraft.text.Text;
 
 
@@ -17,14 +17,21 @@ public class ElytraControl implements ClientModInitializer {
     protected static final KeyBinding elytraToggleKey = new KeyBinding(
             "key." + Constants.MOD_ID + ".elytra_toggle",
             InputUtil.GLFW_KEY_V,
-            KeyBinding.GAMEPLAY_CATEGORY
+            "key.category.elytracontrol"
     );
+
     public static String playerUUID;
+
+    private int elytraTime = 0;
+
     private static final Text ELYTRA_ENABLED_TEXT = Text.translatable("message." + Constants.MOD_ID + ".elytra_enabled");
     private static final Text ELYTRA_DISABLED_TEXT = Text.translatable("message." + Constants.MOD_ID + ".elytra_disabled");
     @Override
     public void onInitializeClient() {
         Constants.LOGGER.info("Elytra Control Initialized");
+
+        KeyBindingHelper.registerKeyBinding(elytraToggleKey);
+
         endClientTickEvent();
     }
 
@@ -43,17 +50,19 @@ public class ElytraControl implements ClientModInitializer {
                 client.inGameHud.setOverlayMessage(elytraToggle ? ELYTRA_ENABLED_TEXT : ELYTRA_DISABLED_TEXT, false);
             }
 
-            while (shouldStopFlying(client)) {
+            if (client.options.jumpKey.isPressed() && client.player.isFallFlying() && elytraTime > 10) {
                 client.player.stopFallFlying();
+                client.player.networkHandler.sendPacket(new ClientCommandC2SPacket(client.player, ClientCommandC2SPacket.Mode.START_FALL_FLYING));
+            }
+            if (client.player.isFallFlying() && !client.options.jumpKey.isPressed()) {
+                elytraTime++;
+                if (elytraTime == 20){
+                    elytraTime = 10;
+                }
+            } else {
+                elytraTime = 0;
             }
 
         });
-    }
-    protected boolean shouldStopFlying(MinecraftClient client) {
-        if (client.player == null) {
-            return false;
-        }
-
-        return client.options.jumpKey.wasPressed() && !client.player.isOnGround() && !client.player.isTouchingWater() && !client.player.hasStatusEffect(StatusEffects.LEVITATION) && client.player.isFallFlying();
     }
 }
